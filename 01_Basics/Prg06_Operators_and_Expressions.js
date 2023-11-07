9117980b4921c98c8d8d14cd06db1c1cb9315834
Ritesh-Kumar-Rai <145654312+Ritesh-Kumar-Rai@users.noreply.github.com>
2023-11-07 13:59:03 +0000

    Adding Prg07 Operators and Expressions file

---
+++ 01_Basics/Prg06_Operators_and_Expressions.js
@@ -0,0 +1,98 @@
+// Operators
+
+
+        //Arithmetic Operators
+
+        console.log("<br><br>Arithmetic Operator<br>");
+        let a = 10;
+        let b = 10;
+        console.log("<br>a+b is ",a+b);
+        console.log("<br>a*b is ",a*b);
+        console.log("<br>a-b is ",a-b);
+        console.log("<br>a/b is ",a/b);
+        console.log("<br>a%b is ",a%b);
+        console.log("<br>a**b is ",a**b);
+
+        console.log("<br><br>Increment/Decrement Operator<br>");
+        //Increment/Decrement Operator Operator (++,--)
+        console.log("<br>++a is ",++a);
+        console.log("<br>a is ",a);
+        console.log("<br>++a is ",a++);
+        console.log("<br>a is ",a);
+
+        //Assignment Operator-> +=, -=, *=, /=, %=, **=, =
+        console.log("<br><br>Assignment Operator<br>");
+
+        console.log("<br>a += ",a+=5);
+        console.log("<br>a += ",a-=5);
+        console.log("<br>a += ",a*=5);
+        console.log("<br>a += ",a/=5);
+        console.log("<br>a += ",a%=5);
+
+        //Comparison Operator
+       
+
+console.log("<h3>Comparison Operators</h3>");
+
+// Greater than operator
+console.log("5 > 3: " + (5 > 3) + "<br>");
+
+// Less than operator
+console.log("5 < 3: " + (5 < 3) + "<br>");
+
+// Greater than or equal to operator
+console.log("5 >= 5: " + (5 >= 5) + "<br>");
+
+// Less than or equal to operator
+console.log("3 <= 5: " + (3 <= 5) + "<br>");
+
+// Equality operator
+console.log("5 == '5': " + (5 == '5') + "<br>");
+
+// Strict equality operator
+console.log("5 === '5': " + (5 === '5') + "<br>");
+
+// Inequality operator
+console.log("5 != '5': " + (5 != '5') + "<br>");
+
+// Strict inequality operator
+console.log("5 !== '5': " + (5 !== '5') + "<br>");
+
+
+// Logical Operators
+
+console.log("<h3>Logical Operators</h3>");
+
+// Logical AND operator
+console.log("(5 > 3) && (7 < 10): " + ((5 > 3) && (7 < 10)) + "<br>");
+
+// Logical OR operator
+console.log("(5 < 3) || (7 < 10): " + ((5 < 3) || (7 < 10)) + "<br>");
+
+// Logical NOT operator
+console.log("!(5 < 3): " + !(5 < 3) + "<br>");
+
+
+// Comments
+
+// This is a single-line comment
+
+/*
+This is a multi-line comment
+that spans multiple lines
+*/
+
+console.log("<h3>Comments</h3>");
+
+// This line will be displayed in the output
+console.log("Hello, world!");
+
+/*
+This code will not be executed:
+
+console.log("This code is commented out and will not be displayed.");
+*/
+
+
+
+
