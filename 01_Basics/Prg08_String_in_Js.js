0aa0c479a55f7a0c56c765e15ca922f5b62f0d06
Ritesh-Kumar-Rai <145654312+Ritesh-Kumar-Rai@users.noreply.github.com>
2023-11-08 19:04:30 +0000

    added new methods in string file named trim and split

--- 01_Basics/Prg08_String_in_Js.js
+++ 01_Basics/Prg08_String_in_Js.js
@@ -31,4 +31,33 @@ console.log(naam.charAt(0));
 // some other string methods
 console.log(naam.substring(0,4));
 console.log(naam.slice(-8,5));
+
+
+// trim() method
+let naam1 = "    Ritesh Rai     ";
+console.log(naam1);
+console.log(naam1.length); // 19
+
+let trimed_naam = naam1.trim();
+
+console.log(trimed_naam)
+console.log(trimed_naam.length) // 10
+
+/*trim() is a commonly used method in JavaScript 
+for removing whitespace from the beginning and end of a string. It can be useful for form validation, 
+especially when validating user input such as email addresses, phone numbers, or passwords. 
+It returns new string with modified data and the actual value is unchanged*/
+
+
+// split() : it is used when you want to break a string into an array of string
+// Example : "Hare Krsna" -> after split() -> ['Hare',' ','Krsna']
+
+/*Syntax : split() accepts two parameters split(separator, limit);
+            - separator (optional) - The pattern (string or regular expression) describing where each split should occur.
+            - limit (optional) - A non-negative integer limiting the number of pieces to split the given string into. */
+
+let naam2 = 'Ritesh Rai'; 
+console.log(naam1.split(''));
+console.log(naam2.split(' '));
+
 //All the above will work for same normal string or string object [new String()]
