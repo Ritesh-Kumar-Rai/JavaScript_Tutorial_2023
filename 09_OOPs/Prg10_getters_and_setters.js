efe8a92a5def34081a0a528218b41c810c766c1a
Ritesh-Kumar-Rai <145654312+Ritesh-Kumar-Rai@users.noreply.github.com>
2024-01-28 12:02:52 +0000

    getters and setters with class, object and functions

---
+++ 09_OOPs/Prg10_getters_and_setters.js
@@ -0,0 +1,24 @@
+class User {
+    constructor(email, password){
+        this.email = email;
+        this.password = password
+    }
+
+    get email(){
+        return this._email.toUpperCase()
+    }
+    set email(value){
+        this._email = value
+    }
+
+    get password(){
+        return `${this._password}hitesh`
+    }
+
+    set password(value){
+        this._password = value
+    }
+}
+
+const ritesh = new User("h@riteshstack.ai", "abc")
+console.log(ritesh.email);
